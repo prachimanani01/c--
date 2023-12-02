2a91024797806c96f17e2950c6cc328f4a548611
prachi <144036679+prachimanani01@users.noreply.github.com>
2023-12-02 12:47:26 +0530

    Add files via upload

---
+++ PR5/pr5(3).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+class vote
+{
+    public:
+        void data()
+        {
+            int age;
+            cout<<"Enter age :-";
+            cin >> age;
+            try{
+                
+                if(age>=18)
+                {
+                    throw "It is eligible for vote";
+                }else{
+                    throw "It is Not eligible for vote";
+                }
+            }
+            catch(const char *age)
+            {
+                cout<< age<<endl;
+            }
+        }
+};
+int main()
+{
+    vote v;
+    v.data();
+
+}
